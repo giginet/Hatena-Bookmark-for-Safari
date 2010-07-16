297e87de4b8ba23b5e8929df6746d0dac808ef65
giginet <giginet.net@gmail.com>
2010-07-17 00:13:22 +0900

    Version:1.1各種設定項目を追加

---
+++ global.js
@@ -0,0 +1,55 @@
+
+var regexps = [];
+//ブラックリストの正規表現リスト
+//指定したURLがブラックリストに含まれるかどうか
+function isDisable(url){
+    flag = false;
+    jQuery.each(regexps,function(){
+        if(this.test(url)){
+            flag=true;
+            break;
+        }
+    });
+    return flag;
+} 
+
+function initAll(){
+    safari.application.addEventListener('message',function(evt){
+        if(evt.name=="api"){
+            if(!isDisable(evt.message)){
+                var api="http://b.hatena.ne.jp/entry/jsonlite/?url="+evt.message;
+                $.getJSON(api,function(data){
+                    evt.target.page.dispatchMessage('api', data);
+                });
+            }else{
+                evt.target.page.dispatchMessage('api', false);
+            }
+            }else if(evt.name=="setting"){
+                setting = safari.extension.settings;
+                //keybindの設定
+                var keys=setting.key.split(",");
+                if(setting.ctrl)keys.unshift("ctrl");
+                if(setting.alt)keys.unshift("alt");
+                if(setting.shift)keys.unshift("shift");
+                var keybind =keys.join("+"); 
+                //無効化するURLリストの作成
+                var urls = setting.disable_url.split(",");
+                jQuery.each(urls,function(){
+                    var reg ="^";
+                    reg +=this.replace("*",".*");
+                    reg +="$";
+                    reg = new RegExp(reg);
+                    regexps.push(reg);
+                });
+                var obj={
+                    "width":setting.width,
+                    "height":setting.height,
+                    "position":setting.position,
+                    "keybind":keybind,
+                    "disable_url":regexps,
+                    "disable":isDisable(evt.message)
+                };
+                evt.target.page.dispatchMessage('setting',obj);
+            }
+        },false);
+    }
---
+++ hb.js
@@ -0,0 +1,118 @@
+if(window.top==window){
+    var post =["","",0,0];
+    var posr =[0,"",0,""];
+    var posl =["",0,"",0];
+    var posb =[0,0,"",""];
+    var flag = false
+    var count = 0;
+    var haveComment = false;
+    var toggleViewer = false;
+    var rooturl = safari.extension.baseURI;
+    //はてぶついか
+    var showAddForm = function(){
+        var base = "http://b.hatena.ne.jp/add"
+        if(location.href.indexOf(base)==-1){
+            var url=base+'?mode=confirm&is_bm=1&title='+escape(document.title)+'&url='+escape(location.href);
+            var popup = $("<div></div>");
+            popup.attr("id","hb-add");
+            var iframe= $("<iframe></iframe>");
+            iframe.attr("src",url);
+            iframe.attr("scrolling","no");
+            popup.append(iframe);
+            iframe.height(500);
+            iframe.width(500);
+            popup.lightbox_me();
+        }
+    }
+    var api;
+    var viewer = $("<div></div>");
+    var tooltip = $("<div></div>");
+    var counter = $("<span></span>");
+    //ツールチップ表示
+    function showTooltip(){
+        counter.css({"background":"url("+rooturl+"hatena.png) no-repeat scroll 0pt 50% transparent"});
+        counter.html(count);
+    }
+    tooltip.bind("mouseenter",function(){
+        if(haveComment && !toggleViewer){
+            toggleViewer = true;
+            viewer.toggle(200);
+            tooltip.toggle(200);
+        }
+    });
+    viewer.bind("mouseleave",function(){
+        viewer.toggle(200);
+        tooltip.toggle(200);
+        toggleViewer = false;
+    });
+    //API読み込み
+    safari.self.addEventListener('message',function(evt){
+        var base = "http://b.hatena.ne.jp/add"
+        var com_count = 0;
+        if(evt.name=="api"){
+            api = evt.message;
+            if(api!=null){
+                viewer.attr("id","HBcommentViewer");
+                //ビュワー表示位置
+                viewer.css("top",post[setting.position]+" !important");
+                viewer.css("right",posr[setting.position]+" !important");
+                viewer.css("left",posl[setting.position]+" !important");
+                viewer.css("bottom",posb[setting.position]+" !important");
+                count = api.count;
+                jQuery.each(api.bookmarks,function(){
+                    var user = this.user;
+                    var time = this.timestamp;
+                    var comment = this.comment;
+                    var tags = this.tags;
+                    var url = "http://b.hatena.ne.jp/"+user+"/";
+                    var image_url = "http://www.st-hatena.com/users/"+user+"/profile_s.gif"
+                    if(comment.length>0){
+                        com_count++;
+                        com = $("<div><a class='HBuser' href='"+url+"' target='_blank'>"+user+"</a>:<span class='HBcomment'>"+comment+"</span><span class='HBtime'>("+time+")</span></div>");
+                        var link = com.children("a.HBuser:first");
+                        link.css({"background":"url("+image_url+") no-repeat scroll 0pt 50% transparent"});
+                        com.attr("class","HBcomment");
+                        viewer.width(setting.width);
+                        viewer.append(com);
+                    }
+                });
+                if(com_count>0){
+                    haveComment = true;
+                    $(document.body).append(viewer);
+                    if(viewer.height()<setting.height){
+                        viewer.height(viewer.height()+10);
+                    }else{
+                        viewer.height(setting.height)
+                    }
+                    viewer.toggle(false);
+                }
+            }
+            showTooltip();
+        }else if(evt.name=="setting"){
+            setting = evt.message;
+            //tooltipの設置
+            if(!setting.disable){
+                tooltip.attr("id","HBtool"); 
+                counter.attr("id","HBcounter");
+                tooltip.append(counter);
+                counter.css({"background":"url("+rooturl+"loading.gif) no-repeat scroll 0pt 50% transparent"});
+                counter.html("loading...");
+                $(document.body).append(tooltip);
+                tooltip.css("top",post[setting.position]+" !important");
+                tooltip.css("right",posr[setting.position]+" !important");
+                tooltip.css("left",posl[setting.position]+" !important");
+                tooltip.css("bottom",posb[setting.position]+" !important");
+            }
+            //キーバインドの設定
+            $(document).bind("keydown",setting.keybind,showAddForm);
+            //はてブAPIの読み込み
+            if(!flag){
+                safari.self.tab.dispatchMessage('api',location.href);
+                flag = true;
+            }
+        }
+    },false);
+    //設定の読み込み
+    var setting;
+    safari.self.tab.dispatchMessage("setting",location.href);
+}
